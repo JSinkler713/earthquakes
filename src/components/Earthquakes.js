54a86dc914d2022d28af75a38b942ddad5a172d1
James Sinker <j@Jamess-MacBook-Pro.local>
2019-12-12 16:18:50 -0800

    changes marker icon image to earthquake

--- src/components/Earthquakes.js
+++ src/components/Earthquakes.js
@@ -1,12 +1,13 @@
 import React from "react";
 
 let Earthquakes = (props)=> {
-    return(
+  let currentSeconds = new Date().getTime()  
+  return(
       <ul>
           {
           props.data.map((item, i) => {
-            return <li key={i}>{item.properties.title}  - {item.properties.time}</li>
-         })
+            return <li key={i}>{item.properties.title}  - {Math.floor((currentSeconds - item.properties.time)/(60*60*1000))} hrs and {Math.floor(Math.floor((currentSeconds - item.properties.time)%(60*60*1000))/(60*1000))} minutes ago</li>
+          })
         }
       </ul>)
     
--- src/components/GoogleMap.js
+++ src/components/GoogleMap.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { Map, GoogleApiWrapper, Marker } from 'google-maps-react';
+import img from './icon.png'
 
 class MapContainer extends Component {
   render() {
@@ -7,6 +8,7 @@ class MapContainer extends Component {
       width: '35%',
       height: '100%'
     }
+    
     return (
       <Map
         style={styles}
@@ -18,7 +20,9 @@ class MapContainer extends Component {
           this.props.data.map((quake, i) => {
             return <
               Marker
+                title={quake.properties.title}
                 key={i}
+                icon={img}
                 id={i}
                 position={{
                   lng: quake.geometry.coordinates[0],  
